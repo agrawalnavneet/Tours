74498ab6f9196cb589120bbbd40feeef7e1538a5
Navneet Agarwal <agrawalnavneet960@gmail.com>
2025-01-30 17:35:14 +0530

    adding modulle2

--- Backend/routes/adminRoutes.js
+++ Backend/routes/adminRoutes.js
@@ -3,4 +3,9 @@ const router = express.Router();
 const Tour = require("../models/tourModel");
 
 router.post("/add-tour", async (req, res) => {
-})
+  const tour = new Tour(req.body);
+  await tour.save();
+  res.status(201).json({ message: "Tour added successfully!" });
+});
+
+module.exports = router;
