7ce991104357bcd15adbc668df395f4dd7f05d04
Navneet Agarwal <agrawalnavneet960@gmail.com>
2025-02-01 16:44:13 +0530

    addded tour routes

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -8,10 +8,5 @@ router.get("/tours", async (req, res) => {
   res.json(tours);
 });
 
-router.post("/book", async (req, res) => {
-  const booking = new Booking(req.body);
-  await booking.save();
-  res.status(201).json({ message: "Booking confirmed!" });
-});
 
 module.exports = router;
