67b18896abfe7d5d726a2ffec72b111f536eb246
Navneet Agarwal <agrawalnavneet960@gmail.com>
2025-02-01 16:43:50 +0530

    addded user routes

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,5 +1,7 @@
 const express = require("express");
-
+const router = express.Router();
+const Tour = require("../models/tourModel");
+const Booking = require("../models/bookingModel");
 
 router.get("/tours", async (req, res) => {
   const tours = await Tour.find();
