9d5e9b12718d6d9825aba21cb4edf2a55e8995fb
Navneet Agarwal <agrawalnavneet960@gmail.com>
2025-03-04 13:52:23 +0530

    booking schema

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,7 @@ import moment from "moment";
 import simpleGit from "simple-git";
 
 const path = "./data.json";
-const date = moment().subtract(140, 'days').format();
+const date = moment().subtract(139, 'days').format();
 
 const commitMessages = [
   " booking schema",
