509da86952948ff9c586fa7075147fe8e331259e
Navneet Agarwal <agrawalnavneet960@gmail.com>
2025-04-08 17:23:30 +0530

    adding Schema

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,7 @@ import moment from "moment";
 import simpleGit from "simple-git";
 
 const path = "./data.json";
-const date = moment().subtract(1, 'days').format();
+const date = moment().subtract(102, 'days').format();
 
 const commitMessages = [
   "adding Schema",
